e353edd2c91859fe56860b151212bd19b01099c2
William Correa <wilcorrea@gmail.com>
2017-11-10 20:11:17 -0200

    Comentários de teste

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import './components'
 
 require('./themes/fonts/index.styl')
 
+/**
+ * Path used to create the component paths into CRUD routes
+ * @type {string}
+ * @private
+ */
 export const __CRUD__ = 'vendor/phpzm/components/crud'
