504515156757821e1ebe4998251fb1d9329ebe55
William Correa <wilcorrea@gmail.com>
2017-11-10 20:45:55 -0200

    Teste de sincronização

---
+++ components/@mixins/MixinNavigation.js
@@ -0,0 +1,33 @@
+import { uniqid } from 'phpzm/support/utils'
+
+export default {
+  methods: {
+    /**
+     * @param {string} path
+     * @param {Object} query
+     */
+    browse (path, query = {}) {
+      let remove = false
+      if (query === false) {
+        query = {}
+      }
+
+      if (query !== undefined) {
+        query = Object.assign({}, this.$route.query, query)
+      }
+      if (query === undefined) {
+        query = {}
+      }
+      if (path === this.$route.path) {
+        query[this.changer] = uniqid()
+      }
+      if (remove) {
+        delete query[this.changer]
+      }
+
+      const browse = () => this.$router.push({path, query})
+
+      window.setTimeout(browse, 100)
+    }
+  }
+}
